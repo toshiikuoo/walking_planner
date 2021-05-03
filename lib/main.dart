cdca749cbe20320a152711268244d6e6edc2555b
toshiikuo <toshiikuo@gmail.com>
2021-05-03 18:46:43 +0900

    地図は動くが、アラートが動作しない。

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 // https://qiita.com/dayjournal/items/4b9f8f8fbdc233abacbf
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 // flutter_mapパッケージ追加
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
@@ -17,8 +18,56 @@ class MapApp extends StatefulWidget {
 
 class _MapAppState extends State<MapApp> {
   String _title = 'map_app';
+  final myController = TextEditingController();
+
+  @override
+  void dispose(){
+    myController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget mapSection = FlutterMap(
+      // マップ表示設定
+      options: MapOptions(
+        center: LatLng(35.681, 139.767),
+        zoom: 14.0,
+      ),
+      layers: [
+        //背景地図読み込み (Maptiler)
+        TileLayerOptions(
+            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            subdomains: ['a', 'b', 'c']
+        ),
+        // サークルマーカー設定
+        CircleLayerOptions(
+          circles: [
+            // サークルマーカー1設定
+            CircleMarker(
+              color: Colors.yellow.withOpacity(0.7),
+              radius: 1000,
+              borderColor: Colors.white.withOpacity(0.9),
+              borderStrokeWidth: 13,
+              point: LatLng(35.681, 139.760),
+              useRadiusInMeter: true,
+            ),
+            // サークルマーカー2設定
+
+          ],
+        ),
+      ],
+    );
+
+    Widget inputSection = TextField(
+      decoration: new InputDecoration(labelText: "Enter your number"),
+      keyboardType: TextInputType.number,
+      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+      controller: myController,
+    );
+
+
+
     return MaterialApp(
       title: 'map_app',
       theme: ThemeData(
@@ -29,36 +78,31 @@ class _MapAppState extends State<MapApp> {
           title: Text(_title),
         ),
         // flutter_map設定
-        body: FlutterMap(
-          // マップ表示設定
-          options: MapOptions(
-            center: LatLng(35.681, 139.767),
-            zoom: 14.0,
-          ),
-          layers: [
-            //背景地図読み込み (Maptiler)
-            TileLayerOptions(
-              urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-              subdomains: ['a', 'b', 'c']
-            ),
-            // サークルマーカー設定
-            CircleLayerOptions(
-              circles: [
-                // サークルマーカー1設定
-                CircleMarker(
-                  color: Colors.yellow.withOpacity(0.7),
-                  radius: 1000,
-                  borderColor: Colors.white.withOpacity(0.9),
-                  borderStrokeWidth: 13,
-                  point: LatLng(35.681, 139.760),
-                  useRadiusInMeter: true,
-                ),
-                // サークルマーカー2設定
+        body: Stack(
+          children: [
+            mapSection,
+            inputSection,],
 
-              ],
-            ),
-          ],
         ),
+        floatingActionButton: FloatingActionButton(
+          // When the user presses the button, show an alert dialog containing
+          // the text that the user has entered into the text field.
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  // Retrieve the text the that user has entered by using the
+                  // TextEditingController.
+                  content: Text("メッセージメッセージメッセージメッセージメッセージメッセージ"),
+                );
+              },
+            );
+          },
+          tooltip: 'Show me the value!',
+          child: Icon(Icons.text_fields),
+        ),
+
       ),
     );
   }
